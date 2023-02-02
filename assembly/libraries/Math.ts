f3b61d68e6f31e0a2e5d050d4e246e5b9cc393e9
ederaleng <ederandreslobo1998@gmail.com>
2023-02-02 09:37:25 -0500

    Removing 0 from 100%

--- assembly/libraries/Math.ts
+++ assembly/libraries/Math.ts
@@ -1,10 +1,10 @@
 import { u128, SafeMath } from "@koinos/sdk-as";
 
-let _100 = u128.fromU64(100000);
+let _100 = u128.fromU64(10000);
 
 export class MathMK {
   static getFeeProtocol(_amount: u64): u64 {
-    let fee = u128.fromU64(2500);
+    let fee = u128.fromU64(250);
     let amount = u128.fromU64(_amount);
     let result = SafeMath.div(SafeMath.mul(amount, fee), _100);
     return result.toU64();
